42585318894aaea43f527dc487cfad85fa994a4d
osho4753 <ramazanmamanov840@gmail.com>
2023-09-13 16:52:49 +0000

    Docker files init

--- backend-blog/validations/auth.js
+++ backend-blog/validations/auth.js
@@ -1,14 +1,17 @@
 import {body} from 'express-validator'
+
 export const registerValidation = [
         body('email','Incorrect Email Format').isEmail(),
         body('password','Incorrect Password Type').isLength({min:5}),
         body('fullName','Incorrect Name Format').isLength({min:3}),
         body('imageUrl','Incorrect Avatar Type').optional().isString(),
 ]
+
 export const loginValidation = [
         body('email','Incorrect Email Format').isEmail(),
         body('password','Incorrect Password Type').isLength({min:5}),
 ]
+
 export const postCreateValidation = [
         body('title','Type Please Title').isLength({min:4}).isString(),
         body('text','Type Please Your Text').isLength({min:3}).isString(),
